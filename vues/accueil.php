fac7b1fbe164c1add0873daa2765bff021d7b392
FlorianLz <46797434+florianlz@users.noreply.github.com>
2019-12-29 16:04:26 +0100

    Avancement page d'accueil.

--- vues/accueil.php
+++ vues/accueil.php
@@ -6,4 +6,46 @@
 
     </div>
     <h1>Plus fort, ensemble !</h1>
+    <div class="cartes">
+        <div class="carte">
+            <a href="#">
+                <div class="masque">
+                    <p>Vers la gallerie de ressources</p>
+                </div>
+            </a>
+        </div>
+        <div class="carte">
+            <a href="#">
+                <div class="masque">
+                    <p>Vers la gallerie de ressources</p>
+                </div>
+            </a>
+        </div>
+        <div class="carte">
+            <a href="#">
+                <div class="masque">
+                    <p>Vers la gallerie de ressources</p>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <section class="but">
+        <h1>En quoi consiste notre campagne ?</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span>Debitis</span> dignissimos, laudantium nulla porro quidem repellat sequi sunt <span>suscipit?</span> Alias architecto aspernatur atque deleniti deserunt dolore dolores enim eveniet fuga, harum id ipsam, iste laborum nobis nostrum obcaecati odio officiis perferendis placeat qui sed sit tempora unde vero voluptatum. Assumenda cupiditate dignissimos, doloremque doloribus enim est <span>laboriosam</span> necessitatibus neque, optio, reiciendis reprehenderit soluta suscipit tenetur! Nemo optio quo vero. Ab amet animi architecto aspernatur consectetur dicta ducimus exercitationem id maxime modi molestias <span>placeat</span>, qui reiciendis sint sunt unde veritatis voluptas voluptatem. A adipisci amet atque aut autem beatae consequatur culpa deleniti dicta dolores eligendi eos est facere, id ipsa iure labore laudantium libero molestiae nesciunt numquam perferendis perspiciatis praesentium quasi quisquam reiciendis repudiandae saepe sapiente sed similique, sint veniam voluptatem voluptates. A aspernatur beatae ducimus earum et ex hic illum iste nemo nisi obcaecati optio porro quae quas quasi recusandae repellat reprehenderit sed tenetur ullam, vel veritatis vero voluptatem. Aliquam distinctio dolores doloribus eligendi error exercitationem fugiat harum id impedit inventore ipsum iusto, labore, molestias nam nemo non nulla numquam perferendis quasi repellat sapiente sint veniam vero. <span>Alias aspernatur consequatur</span>, dolor esse, exercitationem iste nihil numquam odio officiis porro provident reprehenderit. Atque, aut autem blanditiis cum debitis dicta distinctio eius ex, exercitationem fuga iusto laboriosam, laborum neque nesciunt nisi officiis pariatur quis rem repellat reprehenderit soluta tempora temporibus veniam! A accusamus autem cum, deleniti doloremque dolores ducimus explicabo facilis in ipsum libero modi non nostrum, quisquam reiciendis sunt totam ullam veniam.</p>
+    </section>
+
+    <section class="agir">
+        <h1>Comment agir face au harcèlement ?</h1>
+        <div class="descagir">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ex illum minus non quas quo repellendus vel voluptate. Accusamus aliquam animi assumenda, beatae deleniti doloremque eius, esse est fuga illo illum minima, mollitia necessitatibus officia quaerat quisquam tenetur veritatis. Aperiam architecto blanditiis debitis dolor enim eveniet fugit, nesciunt nihil, nulla placeat quibusdam sed ullam unde vero, voluptas? Amet aspernatur at autem blanditiis corporis dolorem fugiat impedit laudantium maxime nesciunt nostrum quaerat qui, quod quos rem similique tenetur. Aut consequuntur deleniti doloremque earum eum hic illo magni nobis quasi suscipit. Aliquam consectetur deleniti, dolorem facere ipsam non omnis quas quis suscipit?</p>
+            <div class="agircontact">
+                <h3>Ou contactez-nous directement</h3>
+                <div class="bouton">
+                    Nous contacter
+                </div>
+            </div>
+        </div>
+    </section>
+
 </div>
