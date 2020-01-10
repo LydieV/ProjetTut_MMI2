2155f8939b76a74a068ed7ed58511c6cf80a0c79
LydieV <49896819+LydieV@users.noreply.github.com>
2020-01-10 10:58:06 +0100

    Update mapage.php
    contenu mapage

--- vues/mapage.php
+++ vues/mapage.php
@@ -1,1 +1,44 @@
-<p>Contenu de ma page perso</p>
+<div class="monprofil">
+    <div class='infoprofil'>
+        <h2> émilie dumant </h2>
+        <div>
+            <div>
+                <p> Email : </p>
+                <p> exemple@gmail.com</p>
+            </div>
+            <p> Vous désirez changer d'adresse mail ?</p>
+        </div>
+
+        <div>
+            <div>
+                <p> Mot de passe : </p>
+                <p> ********</p>
+            </div>
+            <p> Vous désirez changer de mot de passe ?</p>
+        </div>
+        <div>
+            <div>
+                <p> Date de naissance : </p>
+                <p> 30/03/1993 </p>
+            </div>
+        </div>
+
+        <p> Supprimer mon compte </p>
+    </div>
+
+    <div class="activitesprofil">
+        <div>
+            <h3> Les témoignages que j'ai postés </h3>
+            <div></div>
+        </div>
+        <div>
+            <h3> Les témoignages que j'ai sauvegardé </h3>
+            <div></div>
+        </div>
+        <div>
+            <h3> Les commentaires que j'ai posté</h3>
+            <div></div>
+        </div>
+    </div>
+
+</div>
