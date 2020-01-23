cf16d9db82b1aa89684b11bb2ac542aa8d6cc937
LydieV <49896819+LydieV@users.noreply.github.com>
2020-01-23 19:26:34 +0100

    text brut page contact

--- vues/contact.php
+++ vues/contact.php
@@ -1,1 +1,22 @@
-<p>Contenu de la page contact</p>
+<div class="banniere_mapage">
+    <div class="couleur_banniere">
+        <p> Vous avez des questions ? <br/> Une demande de partenariat <br/> Écrivez-nous directement depuis le formulaire. </p>
+    </div>
+</div>
+
+<div>
+    <div>
+        <form method="POST" action="">
+            <label for="email"> Email </label>
+            <input type="email" name="email"/>
+            <label for="email"> Sujet </label>
+            <input type="text" name="sujet"/>
+            <label for="message"> Message </label>
+            <textarea></textarea>
+            <input type="submit" value="Envoyer"/>
+        </form>
+    </div>
+    <div>
+
+    </div>
+</div>
