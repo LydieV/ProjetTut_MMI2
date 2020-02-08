4bee8604e82362da5ac09abe46ff7e3ac79f748a
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-02-08 20:39:09 +0100

    Ajout page 404.

--- vues/404.php
+++ vues/404.php
@@ -1,1 +1,13 @@
-<h1>Page introuvable</h1>
+<div class="contenu404">
+    <div class="banniereaccueil">
+        <p class="p404">404</p>
+    </div>
+    <div class="erreur404">
+        <p>Oops ! La page que vous recherchez n'existe pas ou a été supprimée.</p>
+        <a href="index.php?action=accueil">
+            <div class="btnaccueil">
+                <p>Retour vers l'accueil</p>
+            </div>
+        </a>
+    </div>
+</div>
