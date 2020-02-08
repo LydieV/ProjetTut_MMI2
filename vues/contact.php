7e5679d9af140f3976458f0505202140efa49407
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-02-08 20:40:01 +0100

    Responsive pages contact et mapage.

--- vues/contact.php
+++ vues/contact.php
@@ -1,40 +1,41 @@
-<div class="banniere_mapage">
-    <div class="couleur_banniere">
-        <p> Vous avez des questions ? <br/> Une demande de partenariat ? <br/> Écrivez-nous directement depuis le formulaire. </p>
+<div class="contenucontact">
+    <div class="banniere_mapage">
+        <div class="couleur_banniere">
+            <p> Vous avez des questions ? <br/> Une demande de partenariat ? <br/> Écrivez-nous directement depuis le formulaire. </p>
+        </div>
     </div>
-</div>
 
-<div class="pagecontact">
-    <div>
-        <form method="POST">
-            <label for="email" id="label1"> Email </label>
-            <input type="email" name="email" id="email" onclick="glissement(this.id)"/>
-            <label for="email" id="label2"> Sujet </label>
-            <input type="text" name="sujet" id="sujet" onclick="glissement(this.id)"/>
-            <label for="msg" class="labelmsg"> Message </label>
-            <textarea name="message" id="message"></textarea>
-            <a class="boutonconf" onclick="validateForm()"> Envoyer</a>
-        </form>
+    <div class="pagecontact">
         <div>
-            <p id="status"></p>
+            <form method="POST">
+                <label for="email" id="label1"> Email </label>
+                <input type="email" name="email" id="email" onclick="glissement(this.id)"/>
+                <label for="email" id="label2"> Sujet </label>
+                <input type="text" name="sujet" id="sujet" onclick="glissement(this.id)"/>
+                <label for="msg" class="labelmsg"> Message </label>
+                <textarea name="message" id="message"></textarea>
+                <a class="boutonconf" onclick="validateForm()"> Envoyer</a>
+            </form>
+            <div>
+                <p id="status"></p>
+            </div>
         </div>
-    </div>
-    <div class="infosreseaux">
-        <div class="infonumvert">
-            <p> Numéro vert </p>
-            <p class="numerovert"> 0 800 00 00 01 </p>
-        </div>
-        <div class="reseaux">
-            <p> Vous pouvez aussi consulter nos réseaux sociaux </p>
-            <div class="reseausociaux">
-                <a href="https://www.facebook.com/Parlons-harc%C3%A8lement-103908774511172/?view_public_for=103908774511172">
-                    <img src="img/icone_facebook.png" alt="icone_facebook"/>
-                </a>
-                <a href="https://twitter.com/ParlonsHarclem1">
-                    <img src="img/icone_twitter.png" alt="icone_twitter"/>
-                </a>
+        <div class="infosreseaux">
+            <div class="infonumvert">
+                <p> Numéro vert </p>
+                <p class="numerovert"> 0 800 00 00 01 </p>
+            </div>
+            <div class="reseaux">
+                <p> Vous pouvez aussi consulter nos réseaux sociaux </p>
+                <div class="reseausociaux">
+                    <a href="https://www.facebook.com/Parlons-harc%C3%A8lement-103908774511172/?view_public_for=103908774511172">
+                        <img src="img/icone_facebook.png" alt="icone_facebook"/>
+                    </a>
+                    <a href="https://twitter.com/ParlonsHarclem1">
+                        <img src="img/icone_twitter.png" alt="icone_twitter"/>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
--- vues/mapage.php
+++ vues/mapage.php
@@ -3,91 +3,93 @@
 if(isset($_SESSION['id'])){
 
 ?>
-    <div class="banniere_mapage">
-        <div class="couleur_banniere">
-            <p> Bienvenue sur votre page personnelle. </p>
+    <div class="contenumapage">
+        <div class="banniere_mapage">
+            <div class="couleur_banniere">
+                <p> Bienvenue sur votre page personnelle. </p>
+            </div>
         </div>
-    </div>
 
-    <div class="monprofil">
-        <div class='infosprofil'>
+        <div class="monprofil">
+            <div class='infosprofil'>
 
-            <?php
-            $sql="SELECT * FROM utilisateurs WHERE id=?";
-            $query = $pdo->prepare($sql);
-            $query->execute(array($_SESSION['id']));
-            $infoPsn = $query->fetch();
-            ?>
+                <?php
+                $sql="SELECT * FROM utilisateurs WHERE id=?";
+                $query = $pdo->prepare($sql);
+                $query->execute(array($_SESSION['id']));
+                $infoPsn = $query->fetch();
+                ?>
 
-            <h2 class="ident"> <?php echo $infoPsn['identifiant'] ?> </h2>
-            <form method="POST" action="index.php?action=changeinfo">
-                <input type="hidden" name="changepseudo"/>
-                <input type="submit" class="changementinfo" id="changepseudo" name="modif" value=""/>
-            </form>
-
-            <div class="info">
-                <p> <span class="titreinfo"> Email : </span> <?php echo $infoPsn['email'] ?> </p>
+                <h2 class="ident"> <?php echo $infoPsn['identifiant'] ?> </h2>
                 <form method="POST" action="index.php?action=changeinfo">
-                    <input type="hidden" name="changeemail"/>
-                    <input type="submit" class="changementinfo" name="modif" value="Vous désirez changer d'adresse mail ?"/>
+                    <input type="hidden" name="changepseudo"/>
+                    <input type="submit" class="changementinfo" id="changepseudo" name="modif" value=""/>
+                </form>
+
+                <div class="info">
+                    <p> <span class="titreinfo"> Email : </span> <?php echo $infoPsn['email'] ?> </p>
+                    <form method="POST" action="index.php?action=changeinfo">
+                        <input type="hidden" name="changeemail"/>
+                        <input type="submit" class="changementinfo" name="modif" value="Vous désirez changer d'adresse mail ?"/>
+                    </form>
+                </div>
+
+                <div class="info">
+                    <p> <span class="titreinfo"> Mot de passe : </span> ******* </p>
+                    <form method="POST" action="index.php?action=changeinfo">
+                        <input type="hidden" name="changemdp"/>
+                        <input type="submit" class="changementinfo" name="modif" value="Vous désirez changer de mot de passe ?"/>
+                    </form>
+                </div>
+                <div class="info">
+                    <p> <span class="titreinfo"> Date de naissance : </span> <?php echo $infoPsn['datenaissance'] ?> </p>
+                </div>
+
+                <form method="POST" action="index.php?action=supcompte">
+                    <input type="submit" name="supprimercompte" value="Supprimer mon compte" id="supcompte"/>
                 </form>
             </div>
 
-            <div class="info">
-                <p> <span class="titreinfo"> Mot de passe : </span> ******* </p>
-                <form method="POST" action="index.php?action=changeinfo">
-                    <input type="hidden" name="changemdp"/>
-                    <input type="submit" class="changementinfo" name="modif" value="Vous désirez changer de mot de passe ?"/>
-                </form>
-            </div>
-            <div class="info">
-                <p> <span class="titreinfo"> Date de naissance : </span> <?php echo $infoPsn['datenaissance'] ?> </p>
-            </div>
-
-            <form method="POST" action="index.php?action=supcompte">
-                <input type="submit" name="supprimercompte" value="Supprimer mon compte" id="supcompte"/>
-            </form>
-        </div>
-
-        <div class="activitesprofil">
-            <div class="section_activite">
-                <h3 class="titre1"> Les témoignages que j'ai postés </h3>
-                <div class="carousel_activites">
-                    <?php
-                    $sql = "SELECT * FROM ecrit WHERE idAuteur=?";
-                    $query = $pdo -> prepare($sql);
-                    $query->execute(array($_SESSION['id']));
-                    while($line=$query->fetch()){
-                        $contenu=substr($line['contenu'], 0, 25). ' ...';
-                        echo '<a href="index.php?action=temoignage&id='.$line['id'].'">';
-                            echo '<div class="apercutemoignage">';
-                                echo '<p>"'.$contenu   .'"</p>';
-                            echo '</div>';
-                        echo '</a>';
-                    }
-                    ?>
+            <div class="activitesprofil">
+                <div class="section_activite">
+                    <h3 class="titre1"> Les témoignages que j'ai postés </h3>
+                    <div class="carousel_activites">
+                        <?php
+                        $sql = "SELECT * FROM ecrit WHERE idAuteur=?";
+                        $query = $pdo -> prepare($sql);
+                        $query->execute(array($_SESSION['id']));
+                        while($line=$query->fetch()){
+                            $contenu=substr($line['contenu'], 0, 25). ' ...';
+                            echo '<a href="index.php?action=temoignage&id='.$line['id'].'">';
+                                echo '<div class="apercutemoignage">';
+                                    echo '<p>"'.$contenu   .'"</p>';
+                                echo '</div>';
+                            echo '</a>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="section_activite">
+                    <h3 class="titre2"> Les témoignages que j'ai sauvegardés </h3>
+                    <div class="carousel_activites">
+                        <?php
+                        $sql = "SELECT *, ecrit.id AS idecrit FROM ecrit JOIN sauvegarde ON idUtilisateur=? WHERE sauvegarde.idUtilisateur=? AND sauvegarde.idTemoignage=ecrit.id";
+                        $query = $pdo -> prepare($sql);
+                        $query->execute(array($_SESSION['id'],$_SESSION['id']));
+                        while($line=$query->fetch()){
+                            $contenu=substr($line['contenu'], 0, 25). ' ...';
+                            echo '<a href="index.php?action=temoignage&id='.$line['idecrit'].'"><div class="apercutemoignage"><p>"'.$contenu   .'"</p></div></a>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="section_activite">
+                    <h3 class="titre3"> Les commentaires que j'ai postés </h3>
+                    <div></div>
                 </div>
             </div>
-            <div class="section_activite">
-                <h3 class="titre2"> Les témoignages que j'ai sauvegardés </h3>
-                <div class="carousel_activites">
-                    <?php
-                    $sql = "SELECT *, ecrit.id AS idecrit FROM ecrit JOIN sauvegarde ON idUtilisateur=? WHERE sauvegarde.idUtilisateur=? AND sauvegarde.idTemoignage=ecrit.id";
-                    $query = $pdo -> prepare($sql);
-                    $query->execute(array($_SESSION['id'],$_SESSION['id']));
-                    while($line=$query->fetch()){
-                        $contenu=substr($line['contenu'], 0, 25). ' ...';
-                        echo '<a href="index.php?action=temoignage&id='.$line['idecrit'].'"><div class="apercutemoignage"><p>"'.$contenu   .'"</p></div></a>';
-                    }
-                    ?>
-                </div>
-            </div>
-            <div class="section_activite">
-                <h3 class="titre3"> Les commentaires que j'ai postés </h3>
-                <div></div>
-            </div>
-        </div>
 
+        </div>
     </div>
 
 <?php
