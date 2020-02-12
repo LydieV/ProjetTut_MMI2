765282050c2eacb2073c7f54a8376bb0851e9792
LydieV <49896819+LydieV@users.noreply.github.com>
2020-02-12 17:08:11 +0100

    changement page accueil

--- vues/accueil.php
+++ vues/accueil.php
@@ -9,35 +9,35 @@
         </div>
     </div>
     <h1>Plus fort, ensemble !</h1>
-    <div class="cartes">
+  <!--  <div class="cartes">
         <div class="carte">
             <a href="#">
                 <div class="masque">
-                  <!--  <p>Vers la galerie des ressources</p>-->
+                    <p>Vers la galerie des ressources</p>
                 </div>
             </a>
         </div>
         <div class="carte">
             <a href="#">
                 <div class="masque">
-                  <!--  <p>Vers la galerie des ressources</p>-->
+                   <p>Vers la galerie des ressources</p>
                 </div>
             </a>
         </div>
         <div class="carte">
             <a href="#">
                 <div class="masque">
-                  <!--  <p>Vers la galerie des ressources</p>-->
+                    <p>Vers la galerie des ressources</p>
                 </div>
             </a>
         </div>
-    </div>
+    </div>-->
 
     <section class="but">
         <h1>En quoi consiste notre campagne ?</h1>
         <p>
            <span> Parlons harcèlement </span>  a pour but d’accompagner les victimes de harcèlement afin qu’elle ne se retrouve pas seule.
-            Retrouvez sur ce site l’onglet témoignage (lien vers la page) afin de déposer un témoignage ou de répondre à celui de quelqu’un. <br/>
+            Découvrez sur ce site l’onglet <a href="index.php?action=temoignages" class="lientemoignages"> témoignages </a> afin de déposer un témoignage ou de répondre à celui de quelqu’un. <br/>
             C’est important de ne <span> pas rester seule </span> dans ces moments-là, nous sommes là pour vous écouter et non pour vous juger. <br/>
             Nous organisons des <span>  conférences et des interventions </span> dans des écoles afin de toucher le plus de personne possible.
         </p>
@@ -48,7 +48,7 @@
         <div class="descagir">
             <p>
                 Si vous voyez une personne être victime de hacèlement : agissez !
-                Vous pouvez informer quelqu’un de compétent afin d’arrêter les harceleurs ou contacter le numéro vert : 30 20
+                Vous pouvez informer quelqu’un de compétent afin d’arrêter le ou les harceleur(s) ou contacter le numéro vert : 30 20
                 si vous ne savez pas comment agir. Restez présent pour cette personne, montrez lui que vous êtes là et qu’elle n’est pas seule.
             </p>
             <div class="agircontact">
