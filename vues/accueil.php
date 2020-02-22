f8b361d78e94e34c67ffbfbf7c00c4d1ecc550ae
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2020-02-22 19:52:49 +0100

    Correction fautes d'orthographe.

--- vues/accueil.php
+++ vues/accueil.php
@@ -36,9 +36,9 @@
     <section class="but">
         <h1>En quoi consiste notre campagne ?</h1>
         <p>
-           <span> Parlons harcèlement </span>  a pour but d’accompagner les victimes de harcèlement afin qu’elle ne se retrouve pas seule.
+           <span> Parlons harcèlement </span>  a pour but d’accompagner les victimes de harcèlement afin qu’elle ne se retrouvent pas seules.
             Découvrez sur ce site l’onglet <a href="./temoignages" class="lientemoignages"> témoignages </a> afin de déposer un témoignage ou de répondre à celui de quelqu’un. <br/>
-            C’est important de ne <span> pas rester seule </span> dans ces moments-là, nous sommes là pour vous écouter et non pour vous juger. <br/>
+            C’est important de ne <span> pas rester seul </span> dans ces moments-là, nous sommes là pour vous écouter et non pour vous juger. <br/>
             Nous organisons des <span>  conférences et des interventions </span> dans des écoles afin de toucher le plus de personne possible.
         </p>
     </section>
