589ef1adcf2589f296e2daf707513af65f141b38
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-04-04 16:23:30 +0200

    Maj partie ressources et comrpession des différents visuels.

--- vues/ressources.php
+++ vues/ressources.php
@@ -7,20 +7,26 @@
     <h2 class="titreressource">Affiches promotionnelles de la campagne</h2>
     <div class="conteneuraffiches">
         <img class="affiche" src="./img/visuels/affiche1.jpg" onclick="ouvrirmodale(this.src);">
-        <img class="affiche" src="./img/visuels/affiche2.png" onclick="ouvrirmodale(this.src);">
-       <!-- <img class="affiche" src="./img/visuels/affiche3.png" onclick="ouvrirmodale(this.src);">-->
-      <!--  <img class="affiche" src="./img/visuels/affiche4.png" onclick="ouvrirmodale(this.src);">-->
-     <!--   <img class="affiche" src="./img/visuels/affiche5.png" onclick="ouvrirmodale(this.src);">-->
-        <img class="affiche" src="./img/visuels/affiche6.png" onclick="ouvrirmodale(this.src);">
-        <img class="affiche" src="./img/visuels/affiche7.png" onclick="ouvrirmodale(this.src);">
-    <!--    <img class="affiche" src="./img/visuels/affiche8.png" onclick="ouvrirmodale(this.src);">-->
+        <img class="affiche" src="./img/visuels/affiche2.jpg" onclick="ouvrirmodale(this.src);">
+        <img class="affiche" src="./img/visuels/affiche3.jpg" onclick="ouvrirmodale(this.src);">
+        <img class="affiche" src="./img/visuels/affiche4.jpg" onclick="ouvrirmodale(this.src);">
+        <img class="affiche" src="./img/visuels/affiche5.jpg" onclick="ouvrirmodale(this.src);">
+        <img class="affiche" src="./img/visuels/affiche6.jpg" onclick="ouvrirmodale(this.src);">
+        <img class="affiche" src="./img/visuels/affiche7.jpg" onclick="ouvrirmodale(this.src);">
     </div>
     <div id="fenetremodale" class="modal" onclick="fermerfenetre();">
         <img src="#" id="imagemodale" alt="Apercu de la photo choisie">
     </div>
     <h2 class="titreressource">Flyer de la campagne</h2>
     <div class="conteneurflyer">
-        <img class="flyer" src="./img/visuels/flyer.png" onclick="ouvrirmodale(this.src);">
+        <img class="flyer" src="./img/visuels/recto.jpg" onclick="ouvrirmodale(this.src);">
+        <img class="flyer" src="./img/visuels/verso.jpg" onclick="ouvrirmodale(this.src);">
+    </div>
+    <h2 class="titreressource">Vidéos de la campagne</h2>
+    <div class="conteneurvideos">
+        <iframe class="videoyt" src="https://www.youtube.com/embed/eEfKzPVbFU0" frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
     </div>
     <script src="js/script.js"></script>
 </div>
