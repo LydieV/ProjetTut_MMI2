333f566b2dba788600bf95c3eff18d6cc2a7183c
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-04-17 19:22:11 +0200

    Ajout infos covid page d'accueil.

--- vues/accueil.php
+++ vues/accueil.php
@@ -33,11 +33,27 @@
         </div>
     </div>-->
 
+    <section class="covid">
+        <h1>INFORMATION - COVID-19</h1>
+        <p>
+            En cette période de confinement, le harcèle est toujours présent, mais il peut être différent.
+            C’est pourquoi, durant plusieurs jours, nous allons mener notre étude afin d’analyser l’évolution du harcèlement
+            un mois après le début du confinement. Nous publierons le résultat de notre étude le vendredi 24 avril.
+            Une nouvelle rubrique s’ajoutera à notre site, concernant cette période. <br />
+            <br />
+            En attendant, nous vous invitons à nous suivre les réseaux sociaux avec notre page <a href="https://www.facebook.com/parlonsharcelement/" target="_blank" class="lien">Facebook (@parlonsharcelement)</a>
+            et notre compte <a href="https://twitter.com/ParlonsHarcelmt" target="_blank" class="lien">Twitter (@ParlonsHarcelemt)</a>, afin de suivre nos dernières actualités.<br />
+            <br />
+            On vous souhaite beaucoup de courage durant cette période, et on espère que tout va bien pour vous.
+            Nous restons mobilisés, et si vous avez besoin de quoique ce soit, n’hésitez pas à nous contacter.
+        </p>
+    </section>
+
     <section class="but">
         <h1>En quoi consiste notre campagne ?</h1>
         <p>
            <span> Parlons harcèlement </span>  a pour but d’accompagner les victimes de harcèlement afin qu’elles ne se retrouvent pas seules.
-            Découvrez sur ce site l’onglet <a href="./temoignages" class="lientemoignages" data-pjax> témoignages </a> qui permet de témoigner ou d'interagir aux témoignages d'autres victimes. <br/>
+            Découvrez sur ce site l’onglet <a href="./temoignages" class="lien" data-pjax> témoignages </a> qui permet de témoigner ou d'interagir aux témoignages d'autres victimes. <br/>
             Il est important de ne <span> pas rester seul-e </span> dans ces moments-là, nous sommes présents pour vous écouter et non pour vous juger. <br/>
             Nous organisons des <span>  conférences et des interventions </span> dans des écoles afin de toucher et sensibiliser le plus de personnes possible.
         </p>
